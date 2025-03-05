fcc8ac8e57358429d47b6c186c8d70b6d39b8ce0
Grisell <grisell83@gmail.com>
2025-03-05 16:04:01 +0000

    switch off shape retrieval

--- shape_utils/run.py
+++ shape_utils/run.py
@@ -21,7 +21,7 @@ import io
 
 def main():
     def_no_cpu = min(min(8, multiprocessing.cpu_count()), 8)
-    print(multiprocessing.cpu_count())
+    #print(multiprocessing.cpu_count())
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
@@ -115,6 +115,12 @@ def main():
         help="Calculate minimum distance between the two meshes",
         required=False,
     )
+    parser.add_argument(
+        "--no_shape_retrieval", 
+        action="store_true", 
+        help="switch off shape retrieval calculation",
+        required=False,
+    )
 
     parser.add_argument(
         "--models_zernike",
@@ -124,6 +130,7 @@ def main():
   
     args = parser.parse_args()
     
+
     if args.descr =='WKS':
         parameter_descr = 'energy'
     if args.descr == 'HKS':
@@ -137,89 +144,90 @@ def main():
        mesh2 = TriMesh(args.input_mesh2, area_normalize=False, center=False) 
        min_distance = find_minimum_distance_meshes(mesh1,mesh2)
        print('Minimum distance is:',min_distance)
+    if not args.no_shape_retrieval:
 
-    if args.descr =='WKS'or args.descr == 'HKS':
+        if args.descr =='WKS'or args.descr == 'HKS':
 
-        mesh1 = TriMesh(args.input_mesh1, area_normalize=True, center=False)
-        mesh2 = TriMesh(args.input_mesh2, area_normalize=True, center=False)
+            mesh1 = TriMesh(args.input_mesh1, area_normalize=True, center=False)
+            mesh2 = TriMesh(args.input_mesh2, area_normalize=True, center=False)
 
-        #ouput files with descriptors and parameters list for structure 1 and structure 2
+            #ouput files with descriptors and parameters list for structure 1 and structure 2
 
-        param_list_file = "{}_{}_{}_list.csv".format(parameter_descr,args.descr,pdb_id_1,args.descr,pdb_id_2)
-        descr1_file = "{}_descr_{}.csv".format(args.descr,pdb_id_1)
-        descr2_file = "{}_descr_{}.csv".format(args.descr,pdb_id_2)
+            param_list_file = "{}_{}_{}_list.csv".format(parameter_descr,args.descr,pdb_id_1,args.descr,pdb_id_2)
+            descr1_file = "{}_descr_{}.csv".format(args.descr,pdb_id_1)
+            descr2_file = "{}_descr_{}.csv".format(args.descr,pdb_id_2)
 
-        output_file_1 = os.path.join(args.output,descr1_file)
-        output_file_2 = os.path.join(args.output,descr2_file)
-        output_file_3 = os.path.join(args.output,param_list_file)
+            output_file_1 = os.path.join(args.output,descr1_file)
+            output_file_2 = os.path.join(args.output,descr2_file)
+            output_file_3 = os.path.join(args.output,param_list_file)
 
-        logging.info(f"Calculating {args.descr} descriptors for structures {pdb_id_1} and {pdb_id_2}")
+            logging.info(f"Calculating {args.descr} descriptors for structures {pdb_id_1} and {pdb_id_2}")
 
-        model = functional.FunctionalMapping(mesh1,mesh2)
+            model = functional.FunctionalMapping(mesh1,mesh2)
 
-        if not os.path.exists(output_file_1) or not os.path.exists(output_file_2):
+            if not os.path.exists(output_file_1) or not os.path.exists(output_file_2):
             
-            descr1,descr2,paramlist = calculate_descriptors(model,args.neigvecs,args.n_ev,args.ndescr,args.step,args.landmarks,args.output,args.descr)
+                descr1,descr2,paramlist = calculate_descriptors(model,args.neigvecs,args.n_ev,args.ndescr,args.step,args.landmarks,args.output,args.descr)
         
-            data1 = np.array(descr1)
-            data2 = np.array(descr2)
-            data3 = np.array(paramlist)
+                data1 = np.array(descr1)
+                data2 = np.array(descr2)
+                data3 = np.array(paramlist)
         
-            #save descriptors
-            save_data_to_csv(data1,output_file_1)
-            save_data_to_csv(data2,output_file_2)
+                #save descriptors
+                save_data_to_csv(data1,output_file_1)
+                save_data_to_csv(data2,output_file_2)
 
-            #save parameters list
-            save_list_to_csv(data3,output_file_3)
+                #save parameters list
+                save_list_to_csv(data3,output_file_3)
 
 
-        FM_file = "{}_{}_FM.csv".format(pdb_id_1,pdb_id_2)
-        output_FM = os.path.join(args.output,FM_file)
+            FM_file = "{}_{}_FM.csv".format(pdb_id_1,pdb_id_2)
+            output_FM = os.path.join(args.output,FM_file)
         
-        p2p21_file = "{}_{}_p2p21.csv".format(pdb_id_1,pdb_id_2)
-        output_p2p21 = os.path.join(args.output, p2p21_file)
+            p2p21_file = "{}_{}_p2p21.csv".format(pdb_id_1,pdb_id_2)
+            output_p2p21 = os.path.join(args.output, p2p21_file)
 
-        #compute correspondance matrix, shape difference matrix and p2p21         
+            #compute correspondance matrix, shape difference matrix and p2p21         
         
-        if not os.path.exists(output_FM) or not os.path.exists(output_p2p21):
+            if not os.path.exists(output_FM) or not os.path.exists(output_p2p21):
 
-            p2p21, FM = calculate_functional_maps(model,args.n_cpus,refine = args.refine)
+                p2p21, FM = calculate_functional_maps(model,args.n_cpus,refine = args.refine)
         
-            score_geodesic_norm_eigenvalues = calculate_geodesic_norm_score(FM)
+                score_geodesic_norm_eigenvalues = calculate_geodesic_norm_score(FM)
         
 
-            #save descriptors
-            save_data_to_csv(FM,output_FM)
-            save_list_to_csv(p2p21,output_p2p21)
+                #save descriptors
+                save_data_to_csv(FM,output_FM)
+                save_list_to_csv(p2p21,output_p2p21)
 
-            print('Disimilarity score is:',score_geodesic_norm_eigenvalues)
+                print('Disimilarity score is:',score_geodesic_norm_eigenvalues)
         
-    elif args.descr =='3DZD':
+        elif args.descr =='3DZD':
 
-        if not "map2zernike" and not os.path.isfile(args.map2zernike_binary):
+            if not "map2zernike" and not os.path.isfile(args.map2zernike_binary):
             
-            raise Exception(f"map2zernike binary not found or path not provided: {args.map2zernike_binary}")
+                raise Exception(f"map2zernike binary not found or path not provided: {args.map2zernike_binary}")
 
-        if not "obj2grid" and not os.path.isfile(args.obj2grid_binary):
+            if not "obj2grid" and not os.path.isfile(args.obj2grid_binary):
 
-            raise Exception(f"obj2grid binary not found or path to binary not provided: {args.obj2grid_binary}")
+                raise Exception(f"obj2grid binary not found or path to binary not provided: {args.obj2grid_binary}")
 
-        try:
+            try:
 
-            obj_file_1 = plytoobj(args.input_mesh1,args.output)
-            obj_file_2 = plytoobj(args.input_mesh2,args.output)
-            get_inv(obj_file_1,args.pdb_ids[0],args.map2zernike_binary, args.obj2grid_binary,args.output)
-            get_inv(obj_file_2,args.pdb_ids[1],args.map2zernike_binary, args.obj2grid_binary,args.output)
-            #calculate_similarity_score(args.models_zernike,args.output,atom_type = 'fullatom')
-            #predict_similarity(args.output,args.output,args.models_zernike)
-        except Exception as e:
-            logging.error(
-                "something went wrong, probably map2zernike or obj2grid binaries not working properly  "
-            )
-            logging.error(e)
+                obj_file_1 = plytoobj(args.input_mesh1,args.output)
+                obj_file_2 = plytoobj(args.input_mesh2,args.output)
+                get_inv(obj_file_1,args.pdb_ids[0],args.map2zernike_binary, args.obj2grid_binary,args.output)
+                get_inv(obj_file_2,args.pdb_ids[1],args.map2zernike_binary, args.obj2grid_binary,args.output)
+                #calculate_similarity_score(args.models_zernike,args.output,atom_type = 'fullatom')
+                #predict_similarity(args.output,args.output,args.models_zernike)
+            except Exception as e:
+                logging.error(
+                    "something went wrong, probably map2zernike or obj2grid binaries not working properly  "
+                )
+                logging.error(e)
             
-    else :
-        print('Descriptor not yet implemented')
+        else :
+            print('Descriptor not yet implemented')
     
 
 if __name__ == "__main__":
